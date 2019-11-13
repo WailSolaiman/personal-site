1bc9e0b88449027014d1c302e319fc78c97a2c48
Wail Solaiman <waily2@hotmail.com>
2019-11-13 18:42:32 +0100

    Edited tech. skills

--- src/components/cv/technischeSkills.js
+++ src/components/cv/technischeSkills.js
@@ -9,42 +9,54 @@ const TechnischeSkills = () => {
             <p className="uk-margin-remove-bottom uk-text-bold">
                 Grundkenntnisse:
             </p>
-            <p className="uk-margin-remove">HTML5 und CSS3/SASS.</p>
+            <p className="uk-margin-remove">HTML5, CSS3, jQuery und SASS.</p>
             <p className="uk-margin-remove">
-                Responsive Design mit Bootstrap und uikit.
+                CSS Frameworks: Bootstrap, uikit und Ant Design -> (favourite).
             </p>
             <p className="uk-margin-remove">
                 Entwicklung von Contao-CMS Themes/Templates.
+            </p>
+            <p className="uk-margin-remove">
+                Programmierung dynamischer/responsive Websites mit HTML5, CSS3
+                und JavaScript (ES6).
             </p>
             <p className="uk-margin-remove-bottom uk-text-bold">
                 Gute Kenntnisse:
             </p>
             <p className="uk-margin-remove">
-                Programmierung dynamischer Websites mit JavaScript/ES6, React
-                Framework und MERN Stack (React - MongoDB - Express - Node.js)
+                React Framework: Components (functional and classes), JSX,
+                Props, States, Lifecycle Methods, Events and Hooks.
             </p>
             <p className="uk-margin-remove">
                 Programmierung statischer Websites mit Gatsby Framework (React
                 und GraphQl)
             </p>
             <p className="uk-margin-remove">
-                Entwicklung von Web-Apps mit Hilfe des REST-Prinzips sowie HTTP-
-                und HTTPS-Protokolls.
-            </p>
-            <p className="uk-margin-remove">
-                Serverseitige Programmiersprache: Node.js und MongoDB (Atlas).
-            </p>
-            <p className="uk-margin-remove">
-                Verwendung von Node.js API und verschiedenen Node.js Modules,
-                wie Express, Mongoose, Axios, Bcrypt, JWT und Validator.js.
-            </p>
-            <p className="uk-margin-remove">
-                Entwicklung von Wordpress-CMS Themes.
+                Code schreiben mit Hilfe von Visual Studio Code, Chrome
+                DevTools, Webpack, Nodemon, ESLint, Prettier und Babel.
             </p>
             <p className="uk-margin-remove-bottom uk-text-bold">
                 Basiskenntnisse:
             </p>
-            <p className="uk-margin-remove">Redux Framework</p>
+            <p className="uk-margin-remove">Git.</p>
+            <p className="uk-margin-remove">
+                MERN Stack. (React - MongoDB - Express - Node.js)
+            </p>
+            <p className="uk-margin-remove">
+                Serverseitige Programmiersprache: Node.js.
+            </p>
+            <p className="uk-margin-remove">NoSQL databasee: MongoDB.</p>
+            <p className="uk-margin-remove">
+                Entwicklung von Web-Apps mit Hilfe des REST-Prinzips sowie HTTP-
+                und HTTPS-Protokolls.
+            </p>
+            <p className="uk-margin-remove">
+                Node.js Modules: Express, Mongoose, Axios, Bcrypt, JWT und
+                Validator.js.
+            </p>
+            <p className="uk-margin-remove">
+                Entwicklung von Wordpress-CMS Themes.
+            </p>
         </div>
     )
 }
