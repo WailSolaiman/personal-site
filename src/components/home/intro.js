dfbf006582365235aa83582edb9bf399749cd4d6
Wail Solaiman <wailsolaiman@hotmail.com>
2020-01-07 09:58:35 +0000

    Added two rows for startpage

--- src/components/home/intro.js
+++ src/components/home/intro.js
@@ -2,27 +2,33 @@ import React from 'react'
 
 const Intro = () => {
     return (
-        <div>
-            <h1 className="uk-text-bold uk-text-center uk-margin-small-bottom color-nr4">
-                Webentwickler gesucht?
-            </h1>
-            <p className="uk-text-center color-nr3">
-                Hi, mein Name ist Wail und ich bin seit 2013 als Webentwickler
-                tätig. In dieser Zeit konnte ich bereits vielen Agenturen und
-                deren Kunden bei spannenden Projekten unterstützen. Durch
-                regelmäßige Fortbildung bin ich in der Lage meinen Kunden bzw.
-                Agenturen Webseiten auf Basis der neuesten Webtechnologien zu
-                erstellen.
-            </p>
-            <p className="uk-text-center color-nr3">
-                Dazu gehören Frontend-Technologien wie HTML5, CSS3, SASS,
-                Javascript-Bibliotheken wie jQuery, und Techniken wie Responsive
-                Webdesign. Serverseitig programmiere ich Webapplikationen mit
-                MERN-Stack (React - MongoDB - Express - Node.js) nach
-                MVC-Standards mit dem Framework React. Zu meinen Kompetenzen
-                zählt zudem die Umsetzung dynamischer Webseiten mit Hilfe
-                moderner Content Management Systeme wie Contao und Wordpress.
-            </p>
+        <div uk-grid="">
+            <div className="uk-width-1-1">
+                <h1 className="uk-text-bold uk-text-center uk-margin-small-bottom color-nr4">
+                    Webentwickler gesucht?
+                </h1>
+            </div>
+            <div className="uk-width-1-1 uk-width-1-2@m">
+                <p className="color-nr3">
+                    Mein Name ist Wail. Ich bin seit 2013 als Webentwickler
+                    tätig. In dieser Zeit konnte ich bereits vielen Agenturen und
+                    deren Kunden bei spannenden Projekten unterstützen. Durch
+                    regelmäßige Fortbildung bin ich in der Lage meinen Kunden bzw.
+                    Agenturen Webseiten auf Basis der neuesten Webtechnologien zu
+                    erstellen. Dazu gehören Frontend-Technologien wie HTML5, CSS3, SASS,
+                    Javascript-Bibliotheken wie jQuery, und Techniken wie Responsive
+                    Webdesign.
+                </p>
+            </div>
+            <div className="uk-width-1-1 uk-width-1-2@m">
+                <p className="color-nr3">
+                    Serverseitig programmiere ich Webapplikationen mit
+                    MERN-Stack (React - MongoDB - Express - Node.js) nach
+                    MVC-Standards mit dem Framework React. Zu meinen Kompetenzen
+                    zählt zudem die Umsetzung dynamischer Webseiten mit Hilfe
+                    moderner Content Management Systeme wie Contao und Wordpress.
+                </p>
+            </div>
         </div>
     )
 }
