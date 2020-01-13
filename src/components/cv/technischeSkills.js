dfa0f9723f4edb01d013f3fb02492e257d6fe496
Wail Solaiman <wailsolaiman@hotmail.com>
2020-01-13 14:30:23 +0000

    Updated teschnischeSkills file

--- src/components/cv/technischeSkills.js
+++ src/components/cv/technischeSkills.js
@@ -14,14 +14,19 @@ const TechnischeSkills = () => {
                 CSS Frameworks: Bootstrap, uikit und Ant Design.
             </p>
             <p className="uk-margin-remove">
-                Entwicklung von Contao-CMS Themes/Templates.
+                Responsive Design.
             </p>
             <p className="uk-margin-remove">
-                Programmierung dynamischer/responsive Websites mit HTML5, CSS3
-                und JavaScript (ES6).
+                Umgehen mit der JavaScript-DOM.
+            </p>
+            <p className="uk-margin-remove">
+                Programmierung dynamischer Websites mit JavaScript.
             </p>
             <p className="uk-margin-remove-bottom uk-text-bold">
                 Gute Kenntnisse:
+            </p>
+            <p className="uk-margin-remove">
+                Entwicklung von Contao-CMS Themes/Templates.
             </p>
             <p className="uk-margin-remove">
                 React Framework: Components (functional and classes), JSX,
@@ -62,3 +67,33 @@ const TechnischeSkills = () => {
 }
 
 export default TechnischeSkills
+
+
+/*
+*Technische Fähigkeiten
+Im technischen Sektor gibt es gewisse Grundkenntnisse, die jeder Web Developer beherrschen sollte:
+
+HTML (möglichst alle Versionen inkl. HTML5, XHTML)
+CSS (möglichst alle Versionen inkl. CSS3)
+XML und XSLT
+JavaScript /AJAX
+PHP5 und PHP7
+Prinzipien der objekt-orientierten Programmierung (OOP)
+Je nach Arbeitsschwerpunkt sind dann weitere Kenntnisse erforderlich:
+
+Frontend-Entwickler:
+
+Grundlegende Kenntnisse aus dem Bereich Webdesign
+Responsive Design
+Programmierung dynamischer Websites mit JavaScript und AJAX
+Umgehen mit der JavaScript-DOM-API
+Clientseitige Frameworks und Libaries, z.B. Prototype, AngularJS, ReactJS, UIkit, jQuery, YAML, Bootstrap u.v.m.
+Serverseitige Frameworks und Libraries, z. B. PHP-Symfony, PHP-Laravel, Cake-PHP etc.
+Backend-Entwickler:
+
+Verständnis des HTTP- und HTTPS-Protokolls sowie des REST-Prinzips
+Theorie und Konzepte relationaler Datenbanken inkl. Grundwissen zu den Besonderheiten von Datenbanksystemen wie z. B. Oracle, PostGres, Microsoft SQL und MySQL
+Beherrschung der SQL-Sprache, oft auch XML, JSON
+Serverseitige Programmiersprachen: z.B. Ruby, PHP, Java, C#, serverseitiges JavaScript wie Node.js
+Serverseitige Frameworks: z.B. Ruby on Rails, PHP-Symfony, Zend Framework, Spring. C#/.NET u.a.
+*/
