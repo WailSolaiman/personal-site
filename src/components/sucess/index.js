b33c6bee73b6e61c1e193b5ed2e9059d90eb611c
Wail Solaiman <wailsolaiman@hotmail.com>
2020-03-09 14:50:51 +0100

    edited succes.js component to supprt all languages

---
+++ src/components/sucess/index.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useContext } from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import Head from '../head'
+import HeroImageSmallStyles from '../heroImageSmall'
+import { GlobalStateLanguageContext } from '../../context/GlobalContextLanguageProvider'
+import SuccessMsgStyles from '../../styles/success-msg.module.scss'
+
+const Index = () => {
+  const data = useStaticQuery(graphql`
+    {
+      file(relativePath: { eq: "images/header/header.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1920) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      gcms {
+        personalWebsiteAlls {
+          successMessageHeader
+          successMessageSubheader
+          successMessageButton
+        }
+      }
+    }
+  `)
+  const imageData = data.file.childImageSharp.fluid
+  const state = useContext(GlobalStateLanguageContext)
+  const websiteAllData = data.gcms.personalWebsiteAlls
+  const [successMsgData, setSuccessMsgData] = useState(websiteAllData[0])
+  useEffect(() => {
+    if (state.locale === 'De') {
+      setSuccessMsgData(websiteAllData[0])
+    } else if (state.locale === 'En') {
+      setSuccessMsgData(websiteAllData[1])
+    } else if (state.locale === 'Ar') {
+      setSuccessMsgData(websiteAllData[2])
+    }
+  }, [websiteAllData, state])
+  return (
+    <>
+      <Head title="Success" />
+      <HeroImageSmallStyles imageData={imageData} />
+      <div style={{ minHeight: 300 }}>
+        <h1 className="uk-text-bold uk-text-center uk-margin-small-bottom uk-margin-large-top">
+          {successMsgData.successMessageHeader}
+        </h1>
+        <h2 className="uk-text-bold uk-text-center uk-margin-large-bottom uk-margin-small-top">
+          {successMsgData.successMessageSubheader}
+        </h2>
+        <a
+          className={`uk-button uk-button-default ${SuccessMsgStyles.button}`}
+          href="/">
+          {successMsgData.successMessageButton}
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default Index
